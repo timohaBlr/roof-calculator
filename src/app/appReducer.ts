3c4b61b41b54b07abe0c98958118571b8139d32e
timohaBlr <94timoha@gmail.com>
2023-04-05 08:47:04 +0300

    feat: now data and config right from gitHub Vistegra repository

--- src/app/appReducer.ts
+++ src/app/appReducer.ts
@@ -21,21 +21,33 @@ export const appReducer = (state: AppInitialStateType = appInitialState, action:
     }
 }
 
+function b64_to_utf8(str: string) {
+    return decodeURIComponent(escape(window.atob(str)));
+}
+
 //thunk creators
 export const getDataAndConfigTC = (): AppThunk<AllReducersActionType> => async (dispatch) => {
     try {
         //имитация запроса на сервер
         const headers = {
             'Content-Type': 'application/json',
-            'Accept': 'application/json'
+            'Accept': 'application/vnd.github+json'
         }
+        const baseUrl = 'https://api.github.com/repositories/604581677/contents/data/'
 
-        const response1 = await fetch('data.json', {headers})
-        const response2 = await fetch('config.json', {headers})
-        const result1: DataType[] = await response1.json()
-        const result2: ConfigType[] = await response2.json()
-        dispatch(setDataAC(result1))
-        dispatch(setConfigAC(result2))
+        // const response1 = await fetch('data.json', {headers})
+        // const result1: DataType[] = await response1.json()
+        const response1 = await fetch(`${baseUrl}data.json`, {headers})
+        const result1 = await response1.json()
+        const data: DataType[] = JSON.parse(b64_to_utf8(result1.content))
+
+        // const response2 = await fetch('config.json', {headers})
+        // const result2: ConfigType[] = await response2.json()
+        const response2 = await fetch(`${baseUrl}config.json`, {headers})
+        const result2 = await response2.json()
+        const config: ConfigType[] = JSON.parse(b64_to_utf8(result2.content))
+        dispatch(setDataAC(data))
+        dispatch(setConfigAC(config))
         dispatch(setIsAppInitializedAC(true))
     } catch (err: any) {
         console.warn(err)
